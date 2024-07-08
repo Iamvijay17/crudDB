d7ce69e14b500da208010cb9aae30abfaf372498
Iamvijay17 <vijay9578979255@gmail.com>
2024-07-08 17:43:34 +0530

    cors1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Item = require('./models/item');
 require("dotenv").config();
-app.use(cors());
+// app.use(cors());
 const app = express();
 const PORT = process.env.PORT;
 app.use(bodyParser.json());
